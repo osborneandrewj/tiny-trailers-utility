683d2ddd8907eef9cda5243009f4f01cd7104c47
Zarkwelder <osborne.andrewj@gmail.com>
2017-08-14 16:07:53 -0700

    first commit

---
+++ app/src/main/java/com/example/android/tinytrailersutility/AddMovieActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.tinytrailersutility;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_movie);
+    }
+}
